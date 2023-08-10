5cc1d4df2f9b0f0f8f8d919006e9e3a9d1e63efc
pozdniakova olga <caramelca07@mail.ru>
2023-08-10 12:44:45 +0200

    Added page for adding new products (add_products.html)

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from catalog.views import index, contact, products
+from catalog.views import index, contact, products, add_products
 
 
 urlpatterns = [
     path('', index),
     path('contact/', contact),
     path('products/', products),
+    path('add_products/', add_products),
 ]
